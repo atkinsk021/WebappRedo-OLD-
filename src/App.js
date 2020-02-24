94f5044b8a00c9110a42659f8545856f8cebb0e4
Kevin Atkins <atkinsk021@gmail.com>
2020-02-24 16:10:51 +0000

    initial static version with bootstrap enabled

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/header/";
+import BookmarkList from "./components/bookmarkList/";
+import FilterControls from "./components/filterControls/";
 
 class App extends Component {
   render() {
+    const sample = {
+      title: 'Google',
+      link: 'www.google.com',
+      visits: '112',
+      picture: {thumbnail: './sample.png'}
+  };
+
+    const bookmarks = [sample, sample, sample, sample, sample];
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="jumbotron">
+        <Header noBookmarks={10} />
+        <FilterControls />
+        <BookmarkList bookmarks={bookmarks} />
       </div>
     );
   }
