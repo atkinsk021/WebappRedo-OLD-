61721d8e9207f074b1fd148e1fc63f0b89c200f0
Kevin Atkins <atkinsk021@gmail.com>
2020-02-24 14:13:08 +0000

    Initial header component added

--- stories/index.js
+++ stories/index.js
@@ -1,3 +1,8 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import Header from '../src/components/header/'
+
+storiesOf("Bookmark Vault/Header", module).add("default", () => (
+  <Header noContacts={5} />
+));     
