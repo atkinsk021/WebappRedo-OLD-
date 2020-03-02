b0e5c85f71e31440086d6385e5bd03e59cd97678
Kevin Atkins <atkinsk021@gmail.com>
2020-03-02 16:34:54 +0000

    added routing for crud

---
+++ api/bookmarks/bookmarks.js
@@ -0,0 +1,17 @@
+export const bookmarks = [{
+  'title': 'Google',
+  'link': 'www.google.com',
+},
+{
+  'title': 'Google2',
+  'link': 'www.google2.com',
+},
+{
+  'title': 'Google3',
+  'link': 'www.google3.com',
+},
+{
+  'title': 'Google4',
+  'link': 'www.google4.com',
+},
+];
---
+++ api/bookmarks/index.js
@@ -0,0 +1,61 @@
+import express from 'express';
+import { bookmarks } from './bookmarks';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send({ bookmarks: bookmarks });
+});
+
+//creating bookmark
+router.post('/', (req, res) => {
+        let newBookmark = req.body;
+        if (newBookmark){
+          bookmarks.push({title : newBookmark.title, link: newBookmark.link }) ;
+          res.status(201).send({message: "Bookmark Created"});
+      }else{
+            res.status(400).send({message: "Unable to find Bookmark in request. No Bookmark Found in body"});
+      }
+});
+
+// Update bookmark
+router.put('/:id', (req, res) => {
+  const key = req.params.id;
+  const updateBookmark = req.body;
+  const index = bookmarks.map((bookmark) => {
+    return bookmark.link;
+  }).indexOf(key);
+  if (index !== -1) {
+    bookmarks.splice(index, 1, {
+      title: updateBookmark.title,
+      link: updateBookmark.link
+    });
+    res.status(200).send({
+      message: 'Bookmark Updated'
+    });
+  } else {
+    res.status(400).send({
+      message: 'Unable to find bookmark in request. No bookmark Found in body'
+    });
+  }
+});
+
+// Delete bookmark
+router.delete('/:id', (req, res) => {
+  const key = req.params.id;
+  const index = bookmarks.map((bookmark) => {
+    return bookmark.link;
+  }).indexOf(key);
+  if (index > -1) {
+    bookmarks.splice(index, 1);
+    res.status(200).send({
+      message: `Deleted bookmark with link of: ${key}.`
+    });
+  } else {
+    res.status(400).send({
+      message: `Unable to find bookmark with link of: ${key}.`
+    });
+  }
+});
+
+export default router;
